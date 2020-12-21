53d26d4ffb992ece6db60f1a61343e29e9de03a9
ewspaan <ewspaan@gmail.com>
2020-12-21 22:32:16 +0100

    Button disable poging

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
+import React,{useState} from 'react';
 import './App.css';
+import PokemonList from "./components/pokemonList/PokemonList";
+import ButtonBar from "./components/buttonBar/ButtonBar";
+import Logo from "../src/assets/Pokemon-Logo-PNG4.png";
+import Button from "./components/button/Button";
 
 function App() {
+    const [upOrDown, setUpOrDown] = useState(0);
+
+    function previous(){
+        if (upOrDown > 0){
+            setUpOrDown(-1)
+        }
+        else {
+            setUpOrDown(upOrDown - 1);
+        }
+    }
+
+    function next() {
+        if (upOrDown < 0) {
+            setUpOrDown(1)
+        }
+        else {
+            setUpOrDown(upOrDown + 1);
+        }
+    }
+
     return (
-        <div className="App">
-            {/* Hier komt de magie ✨✨✨✨ */}
+        <div>
+            <img src={Logo} alt="Pokemon Logo"/>
+            <ButtonBar>
+                <Button onClick={previous}>Vorige</Button>
+                <Button onclick={next}>Volgende</Button>
+            </ButtonBar>
+            <PokemonList previousOrNext={upOrDown}/>
         </div>
     );
 }
